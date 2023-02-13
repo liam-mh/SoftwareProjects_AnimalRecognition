ec0152f00e9a61c18107b4a2a82a2caa951e1634
Shaq03 <93329271+Shaq03@users.noreply.github.com>
2023-02-13 15:59:15 +0000

    index update
    changed the landing page and connecting to login page

--- server/admin-login.js
+++ server/admin-login.js
@@ -15,3 +15,11 @@ else
 
 
 }
+
+const menu = document.querySelector('#mobile-menu');
+const menuLinks = document.querySelector('.navbar__menu');
+
+menu.addEventListener('click', function() {
+  menu.classList.toggle('is-active');
+  menuLinks.classList.toggle('active');
+});
