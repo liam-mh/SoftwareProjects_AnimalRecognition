98d553ba955423122489804d1e70f68b54ed24c7
Liam <33222279+liam-mh@users.noreply.github.com>
2023-02-14 15:29:20 +0000

    simplified formatting data
    Writes image data to currentSearch.json in correct format with reduced code

--- server/dataStore/currentSearch.js
+++ server/dataStore/currentSearch.js
@@ -4,63 +4,26 @@
 
 const fs = require('fs');
 
-function writeToFile(filePath, data) {
+function writeToFile(fileName, data) {
     try {
-        fs.writeFileSync(filePath, JSON.stringify(data), 'utf-8');
-        console.log('Data saved to: ', filePath);
+        fs.writeFileSync('./dataStore/'+fileName, JSON.stringify(data), 'utf-8');
+        console.log('Data saved to:', fileName);
     } catch (error) {
         console.error(error);
     }
 }
 
-function addData(userImages) {
-
-    const dataArray = [];
-    const currentDate = new Date();
-
-    userImages.forEach(element => {
-        const data = {
-            date: currentDate,
-            path: element.path,
-            labels: element.labels.map(label => ({
-                valid: true,
-                description: label.description,
-                score: label.score
-            }))
-        };
-        console.log(data);
-        dataArray.push(data);
-    });
-
-    writeToFile('./dataStore/currentSearch.json', dataArray);
-};
-
-module.exports = addData;
+module.exports = writeToFile;
 
 /**
  * Updating the data with the user validated labels
  * Using dummy data until passed from front-end
  */
 
-function readFromFile(filePath) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(filePath, (err, data) => {
-            if (err) reject(err);
-            try {
-                const jsonData = JSON.parse(data || "{}");
-                resolve(jsonData);
-            } catch (err) {
-                reject(err);
-            }
-        });
-    });
-}
   
-firstSearchData = readFromFile('dataStore/currentSearch.json').then((jsonData) => {
-    console.log(JSON.stringify(jsonData, null, 4));
-}).catch((err) => {
-    console.error(err);
-});
+
+
+
 
 
   
