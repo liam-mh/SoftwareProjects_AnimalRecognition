d5929b32a1e2fc9db64ccea8179498c5ad608bf4
Liam <33222279+liam-mh@users.noreply.github.com>
2023-02-14 12:50:10 +0000

    format labels

--- server/dataStore/currentSearch.js
+++ server/dataStore/currentSearch.js
@@ -1,5 +1,17 @@
+/**
+ * Receiving, formatting and saving the users current images with the search data
+ */
 
 const fs = require('fs');
+
+function writeToFile(filePath, data) {
+    try {
+        fs.writeFileSync(filePath, JSON.stringify(data), 'utf-8');
+        console.log('Data saved to: ', filePath);
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 function addData(userImages) {
 
@@ -11,29 +23,44 @@ function addData(userImages) {
             date: currentDate,
             path: element.path,
             labels: element.labels.map(label => ({
+                valid: true,
                 description: label.description,
                 score: label.score
             }))
         };
+        console.log(data);
         dataArray.push(data);
     });
 
-    try {
-        fs.writeFileSync('./dataStore/currentSearch.json', JSON.stringify(dataArray), 'utf-8');
-        console.log('Current search data saved');
-    } catch (error) {
-        console.error(error);
-    }
-    
+    writeToFile('./dataStore/currentSearch.json', dataArray);
 };
 
 module.exports = addData;
 
-
 /**
- fs.readFile('data.json', (err, data) => {
-        if (err) throw err;
-        let jsonData = JSON.parse(data);
-        console.log(JSON.stringify(jsonData, null, 4));
-    });
+ * Updating the data with the user validated labels
+ * Using dummy data until passed from front-end
  */
+
+function readFromFile(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, (err, data) => {
+            if (err) reject(err);
+            try {
+                const jsonData = JSON.parse(data || "{}");
+                resolve(jsonData);
+            } catch (err) {
+                reject(err);
+            }
+        });
+    });
+}
+  
+firstSearchData = readFromFile('dataStore/currentSearch.json').then((jsonData) => {
+    console.log(JSON.stringify(jsonData, null, 4));
+}).catch((err) => {
+    console.error(err);
+});
+
+
+  
