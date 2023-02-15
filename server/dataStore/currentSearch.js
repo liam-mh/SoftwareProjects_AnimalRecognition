1a2c25e22cf5dc2473ace6efb63eaacb0ac8ea45
Liam <33222279+liam-mh@users.noreply.github.com>
2023-02-15 09:30:37 +0000

    Compare user validation - WIP

--- server/dataStore/currentSearch.js
+++ server/dataStore/currentSearch.js
@@ -20,7 +20,57 @@ module.exports = writeToFile;
  * Using dummy data until passed from front-end
  */
 
-  
+function readJsonFileToArray(fileName) {
+    try {
+        const jsonData = fs.readFileSync('./'+fileName, 'utf-8');
+        const data = JSON.parse(jsonData);
+        return data;
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+const dataFromFile = readJsonFileToArray('currentSearch.json');
+
+dummyData = 
+[
+    {
+        "path":"cat_dog.jpg",
+        "labels":[
+            {
+                "description":"Dog",
+                "userThinksValid":false
+            }
+        ],
+    }
+]
+
+function updateUserValidation(original, update) {
+
+    for (let i = 0; i < original.length; i++) {
+
+        var o = original[i];
+        var u = update[i];
+        if (o.path != u.path) {continue;}
+
+        for (let j = 0; j < original[i].labels.length; j++) {
+
+            o = o.labels[j];
+            u = u.labels[j];
+
+            if (!o || !u) {
+                continue;
+            } 
+            if (o.description === u.description) {
+                o.userThinksValid = u.userThinksValid;
+            }
+            console.log('o: ', o.userThinksValid, ' u: ', u.userThinksValid); 
+        }
+    }
+};
+
+
+updateUserValidation(dataFromFile, dummyData);
 
 
 
